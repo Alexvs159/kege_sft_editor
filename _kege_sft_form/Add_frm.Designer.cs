5246986100ac209ef8f5a0e0b7354c4087c8dfe5
Alexvs159 <alexvs159@gmail.com>
2024-05-19 18:23:18 +1100

    редактирование формы добавления

--- _kege_sft_form/Add_frm.Designer.cs
+++ _kege_sft_form/Add_frm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Add_frm));
             this.add_btn = new System.Windows.Forms.Button();
             this.type = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -41,11 +42,11 @@
             // 
             // add_btn
             // 
-            this.add_btn.Location = new System.Drawing.Point(97, 273);
+            this.add_btn.Location = new System.Drawing.Point(70, 211);
             this.add_btn.Name = "add_btn";
             this.add_btn.Size = new System.Drawing.Size(75, 23);
             this.add_btn.TabIndex = 0;
-            this.add_btn.Text = "button1";
+            this.add_btn.Text = "Сохранить";
             this.add_btn.UseVisualStyleBackColor = true;
             this.add_btn.Click += new System.EventHandler(this.add_btn_Click);
             // 
@@ -60,7 +61,7 @@
             "IDE",
             "TextEditor",
             "Spreadsheet"});
-            this.type.Location = new System.Drawing.Point(97, 33);
+            this.type.Location = new System.Drawing.Point(70, 12);
             this.type.Name = "type";
             this.type.Size = new System.Drawing.Size(121, 21);
             this.type.TabIndex = 1;
@@ -68,7 +69,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(65, 36);
+            this.label1.Location = new System.Drawing.Point(34, 20);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(26, 13);
             this.label1.TabIndex = 4;
@@ -77,7 +78,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(56, 108);
+            this.label2.Location = new System.Drawing.Point(25, 73);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 5;
@@ -85,14 +86,14 @@
             // 
             // name
             // 
-            this.name.Location = new System.Drawing.Point(97, 167);
+            this.name.Location = new System.Drawing.Point(70, 140);
             this.name.Name = "name";
             this.name.Size = new System.Drawing.Size(121, 20);
             this.name.TabIndex = 6;
             // 
             // version
             // 
-            this.version.Location = new System.Drawing.Point(97, 197);
+            this.version.Location = new System.Drawing.Point(70, 173);
             this.version.Name = "version";
             this.version.Size = new System.Drawing.Size(121, 20);
             this.version.TabIndex = 7;
@@ -100,7 +101,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(34, 170);
+            this.label3.Location = new System.Drawing.Point(3, 143);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(57, 13);
             this.label3.TabIndex = 8;
@@ -109,7 +110,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(47, 200);
+            this.label4.Location = new System.Drawing.Point(16, 176);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(44, 13);
             this.label4.TabIndex = 9;
@@ -125,7 +126,7 @@
             "Python",
             "Pascal",
             "Sal"});
-            this.langs.Location = new System.Drawing.Point(97, 67);
+            this.langs.Location = new System.Drawing.Point(70, 39);
             this.langs.Name = "langs";
             this.langs.Size = new System.Drawing.Size(120, 94);
             this.langs.TabIndex = 10;
@@ -134,7 +135,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(312, 331);
+            this.ClientSize = new System.Drawing.Size(249, 264);
             this.Controls.Add(this.langs);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -144,8 +145,9 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.type);
             this.Controls.Add(this.add_btn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Add_frm";
-            this.Text = "Add_frm";
+            this.Text = "Добавить";
             this.ResumeLayout(false);
             this.PerformLayout();
 
